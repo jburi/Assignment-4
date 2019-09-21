772569f98963098c2309de04270755aee1f3b5be
jburi <jacobbob55@comcast.net>
2019-09-21 10:05:55 -0500

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Jacob Buri
+ * PlayerControllerX.cs
+ * Assignment 4
+ * Gets spacebar to spawn a dog
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float nextFire = 0.0f;
+    public float fireRate = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press and after cd, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject clone = Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation) as GameObject;
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Jacob Buri
+ * SpawnManagerX.cs
+ * Assignment 4
+ * Spawns balls at the top of the screen in three colors
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    //Create ball color index
+    private int[] colors = new int[3] { 0, 1, 2};
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(3.0f, 5.0f));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        int ballColor = colors[Random.Range(0, 2)];
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballColor], spawnPos, ballPrefabs[ballColor].transform.rotation);
+    }
+
+}
